9bdae2ed1072baed7c29a6d087b3def72d3bb18c
masa <pikorlov@gmail.com>
2021-04-29 21:52:35 +0800

    Update task_6

--- task_6.py
+++ task_6.py
@@ -1,10 +1,13 @@
 def int_func(text):
-    return text.title()
+    new_text = []
+    for word in text.split(' '):
+        new_text.append(word.capitalize())
+    return ' '.join(new_text)
 
 
 first_text = 'fIrSt'
 print(int_func(first_text))
 
 # user_text = input('Введите несколько слов из латинских букв разделенных пробелом: ')
-user_text = 'First second third'
+user_text = "First second's third"
 print(int_func(user_text))
